acdbd05e8aca9756f5e47ea67916d071210a866d
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2024-08-08 11:52:18 +0530

    test

--- src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
+++ src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
@@ -3,9 +3,13 @@ package com.javatechie.redis;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,47 +30,91 @@ public class SpringDataRedisExampleApplication {
 	@Autowired
     private RedisTemplate<String, String> redisTemplate;
 	
+    private final Lock lock = new ReentrantLock();
+	
 	String queueNames = "q1,q2,q3,q4,q5,q6"; //for testing, will need to pass values from DB
 
-    @GetMapping("/getNextEntry")
-    public String getNextEntry(@RequestParam String disseminationProfileId) {
-    	ListOperations<String, String> listOps = redisTemplate.opsForList();
-        List<String> cachedQueues = listOps.range("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId, 0, -1);
-        
-        String[] splitedQueues = queueNames.split(",");
-        List<String> listOfQueues = new ArrayList<>(Arrays.asList(splitedQueues));
-        
-        if(!cachedQueues.isEmpty() && deleteCacheIfNecessary(disseminationProfileId, cachedQueues, listOfQueues)) 
-    		return roundRobinService.getNextEntry(disseminationProfileId, cachedQueues);
-    	else {
-    		resetLastAccessedIdx(disseminationProfileId);  // reset counter if queues doesn't exist 
-            cachedQueues = roundRobinService.initializeCache(disseminationProfileId, listOfQueues);
-            return roundRobinService.getNextEntry(disseminationProfileId, cachedQueues);
-    	}
-    }
-    
-    private Boolean deleteCacheIfNecessary(String disseminationProfileId, List<String> cachedQueues, List<String> listOfQueues) {
-    	Boolean compareQueueNames = compareQueueNames(cachedQueues, listOfQueues);
-        if (!compareQueueNames) {
-            // Delete the cache
-        	System.out.println("cache Deleted...");
-            redisTemplate.delete("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId);
+	@GetMapping("/getNextEntry")
+	public String getNextEntry(@RequestParam String disseminationProfileId) {
+	    lock.lock();
+	    try {
+	        ListOperations<String, String> listOps = redisTemplate.opsForList();
+	        List<String> cachedQueues = listOps.range("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId, 0, -1);
+
+	        String[] splitedQueues = queueNames.split(",");
+	        List<String> listOfQueues = new ArrayList<>(Arrays.asList(splitedQueues));
+
+	        if (cachedQueues == null || cachedQueues.isEmpty() || !deleteCacheIfNecessary(disseminationProfileId, cachedQueues, listOfQueues)) {
+	            resetLastAccessedIdx(disseminationProfileId); // reset counter if queues don't exist
+	            cachedQueues = roundRobinService.initializeCache(disseminationProfileId, queueNames);
+	        }
+	        return roundRobinService.getNextEntry(disseminationProfileId, cachedQueues);
+	    }catch (DataAccessException e) {
+            // Handle Redis-specific exceptions
+	    	System.out.println("Error accessing Redis cache:" + e);
+            throw new RuntimeException("Error accessing Redis cache", e); // Or rethrow a custom exception
         }
-        return compareQueueNames;
-    }
-    
-    private void resetLastAccessedIdx(String disseminationProfileId) {
-    	String counterKey = "ISO_CACHE_DISSEMINATION_ROUNDROBIN_LAST_ACCESSED_IDX_" + disseminationProfileId;
-        if (redisTemplate.hasKey(counterKey)) {
-        	System.out.println("cache reset...");
-            redisTemplate.opsForValue().set(counterKey, "0");
+	    catch (Exception e) {
+	        // Handle general exceptions
+            System.out.println("Error retrieving next entry" + e);
+	        throw new RuntimeException("Error retrieving next entry", e);
+	    }  finally {
+	        lock.unlock();
+	    }
+	}
+
+	private Boolean deleteCacheIfNecessary(String disseminationProfileId, List<String> cachedQueues, List<String> listOfQueues) {
+	    lock.lock();
+	    try {
+	        Boolean compareQueueNames = compareQueueNames(cachedQueues, listOfQueues);
+	        if (!compareQueueNames) {
+	            // Delete the cache
+	            System.out.println("cache Deleted...");
+	            redisTemplate.delete("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId);
+	        }
+	        return compareQueueNames;
+	    } catch (DataAccessException e) {
+            // Handle Redis-specific exceptions
+	    	System.out.println("Error accessing Redis cache:" + e);
+            throw new RuntimeException("Error accessing Redis cache", e); // Or rethrow a custom exception
         }
-    }
+	    catch (Exception e) {
+	        // Handle general exceptions
+            System.out.println("Error retrieving next entry" + e);
+	        throw new RuntimeException("Error retrieving next entry", e);
+	    }  finally {
+	        lock.unlock();
+	    }
+	}
+
+	private void resetLastAccessedIdx(String disseminationProfileId) {
+	    lock.lock();
+	    try {
+	        String counterKey = "ISO_CACHE_DISSEMINATION_ROUNDROBIN_LAST_ACCESSED_IDX_" + disseminationProfileId;
+	        if (redisTemplate.hasKey(counterKey)) {
+	            System.out.println("cache reset...");
+	            redisTemplate.opsForValue().set(counterKey, "0");
+	        }
+	    } catch (DataAccessException e) {
+            // Handle Redis-specific exceptions
+	    	System.out.println("Error accessing Redis cache:" + e);
+            throw new RuntimeException("Error accessing Redis cache", e); // Or rethrow a custom exception
+        }
+	    catch (Exception e) {
+	        // Handle general exceptions
+            System.out.println("Error retrieving next entry" + e);
+	        throw new RuntimeException("Error retrieving next entry", e);
+	    }  finally {
+	        lock.unlock();
+	    }
+	}
+
+	private Boolean compareQueueNames(List<String> cachedQueues, List<String> listOfQueues) {
+	    System.out.println("cache compare...");
+	    return cachedQueues.equals(listOfQueues);
+	}
+
     
-    private Boolean compareQueueNames(List<String> cachedQueues, List<String> listOfQueues) {
-    	System.out.println("cache compare...");
-    	return cachedQueues.equals(listOfQueues);
-    }
     
     /*
     @GetMapping("/getNextEntry")
--- src/main/java/com/javatechie/redis/respository/RoundRobinService.java
+++ src/main/java/com/javatechie/redis/respository/RoundRobinService.java
@@ -1,44 +1,94 @@
 package com.javatechie.redis.respository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.support.atomic.RedisAtomicInteger;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 public class RoundRobinService {
 
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
-    
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
-    
-    public List<String> initializeCache(String disseminationProfileId, List<String> listOfQueues) {
-        ListOperations<String, String> listOps = redisTemplate.opsForList();
-        listOps.rightPushAll("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId, listOfQueues);
-        return listOfQueues;
-    }
+	 	@Autowired
+	    private RedisTemplate<String, String> redisTemplate;
 
+	    @Autowired
+	    private RedisConnectionFactory redisConnectionFactory;
 
-    public String getNextEntry(String disseminationProfileId, List<String> cachedQueues) {
-    	 if (cachedQueues != null && !cachedQueues.isEmpty()) {
-             String counterKey = "ISO_CACHE_DISSEMINATION_ROUNDROBIN_LAST_ACCESSED_IDX_" + disseminationProfileId;
-             RedisAtomicInteger counter = new RedisAtomicInteger(counterKey, redisConnectionFactory);
-             System.out.println("counter: "+counter);
-             int index = counter.getAndIncrement() % cachedQueues.size();
-             if(counter.get() >= 20) 
-            	 counter.set(0); // Reset counter to prevent overflow
+	    private final Map<String, RedisAtomicInteger> counterCache = new ConcurrentHashMap<>();
+	    private final Lock redisTemplateLock = new ReentrantLock();
+	    private final Lock counterCacheLock = new ReentrantLock();
 
-             return cachedQueues.get(index);
-         }
-    	return "";
- 
-    }
+	    public List<String> initializeCache(String disseminationProfileId, String queueNames) {
+	        ListOperations<String, String> listOps = redisTemplate.opsForList();
+	        String[] splitedQueues = queueNames.split(",");
+	        List<String> listOfQueues = new ArrayList<>(Arrays.asList(splitedQueues));
+	        try {
+	            redisTemplateLock.lock();
+	            try {
+	                listOps.rightPushAll("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId, listOfQueues);
+	            } finally {
+	                redisTemplateLock.unlock();
+	            }
+	        } catch (DataAccessException | IndexOutOfBoundsException | ConcurrentModificationException e) {
+	            System.out.println("Error initializing cache:" + e);
+	            throw new RuntimeException("Error initializing cache", e);
+	        } catch (Exception e) {
+	            System.out.println("Unexpected error:" + e);
+	            throw e;
+	        }
+	        return listOfQueues;
+	    }
+
+	    public String getNextEntry(String disseminationProfileId, List<String> cachedQueues) {
+	        if (cachedQueues == null || cachedQueues.isEmpty()) {
+	            return "";
+	        }
+
+	        String counterKey = "ISO_CACHE_DISSEMINATION_ROUNDROBIN_LAST_ACCESSED_IDX_" + disseminationProfileId;
+	        RedisAtomicInteger counter;
+
+	        counterCacheLock.lock();
+	        try {
+	        	 if (redisTemplate.hasKey(counterKey) == Boolean.FALSE) {
+	                 counterCache.remove(counterKey);
+	             }
+	            counter = counterCache.computeIfAbsent(counterKey, k -> new RedisAtomicInteger(k, redisConnectionFactory));
+	            //System.out.println("counter: " + counter);
+	        } finally {
+	            counterCacheLock.unlock();
+	        }
+
+	        try {
+	            counterCacheLock.lock();
+	            try {
+	                int index = counter.getAndIncrement() % cachedQueues.size();
+	                if (counter.get() >= (Integer.MAX_VALUE / 2)) {
+	                    counter.compareAndSet(counter.get(), 0);
+	                }
+	                return cachedQueues.get(index);
+	            } finally {
+	                counterCacheLock.unlock();
+	            }
+	        } catch (DataAccessException | IndexOutOfBoundsException | ConcurrentModificationException e) {
+	            System.out.println("Error getting next entry:" + e);
+	            throw new RuntimeException("Error getting next entry", e);
+	        } catch (Exception e) {
+	            System.out.println("Unexpected error:" + e);
+	            throw e;
+	        }
+	    }
     
     /*public Map<Object, Object> initializeCache(String disseminationProfileId, String queueNames) {
     	
