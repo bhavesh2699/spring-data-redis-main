1c3d181f54ab46dc7dff6ae2b0a7839c3b1f3e52
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2024-08-09 16:16:14 +0530

    test

--- src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
+++ src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
@@ -95,5 +95,95 @@ public class SpringDataRedisExampleApplication {
             throw new RuntimeException("Error retrieving next entry", e);
         }
     }
+	
+	@GetMapping("/deleteQueue")
+    @CacheEvict(value = "ISO_CACHE_DISSEMINATION_ROUNDROBIN", key = "'ISO_CACHE_DISSEMINATION_ROUNDROBIN_' + #disseminationProfileId")
+    public String deleteQueue(@RequestParam String disseminationProfileId) {
+        return "Queue deleted from cache";
+    }
+	
+	@GetMapping("/deleteCounter")
+    @CacheEvict(value = "ISO_CACHE_DISSEMINATION_ROUNDROBIN", key = "'ISO_CACHE_DISSEMINATION_ROUNDROBIN_LAST_ACCESSED_IDX_' + #disseminationProfileId")
+    public String deleteCounter(@RequestParam String disseminationProfileId) {
+        return "Counter deleted from cache";
+    }
+	
+	
+
+	/*private Boolean deleteCacheIfNecessary(String disseminationProfileId, List<String> cachedQueues, List<String> listOfQueues) {
+	    lock.lock();
+	    try {
+	        Boolean compareQueueNames = compareQueueNames(cachedQueues, listOfQueues);
+	        if (!compareQueueNames) {
+	            // Delete the cache
+	            System.out.println("cache Deleted...");
+	            redisTemplate.delete("ISO_CACHE_DISSEMINATION_ROUNDROBIN_" + disseminationProfileId);
+	        }
+	        return compareQueueNames;
+	    } catch (DataAccessException e) {
+            // Handle Redis-specific exceptions
+	    	System.out.println("Error accessing Redis cache:" + e);
+            throw new RuntimeException("Error accessing Redis cache", e); // Or rethrow a custom exception
+        }
+	    catch (Exception e) {
+	        // Handle general exceptions
+            System.out.println("Error retrieving next entry" + e);
+	        throw new RuntimeException("Error retrieving next entry", e);
+	    }  finally {
+	        lock.unlock();
+	    }
+	}
+
+	private void resetLastAccessedIdx(String disseminationProfileId) {
+	    lock.lock();
+	    try {
+	        String counterKey = "ISO_CACHE_DISSEMINATION_ROUNDROBIN_LAST_ACCESSED_IDX_" + disseminationProfileId;
+	        if (redisTemplate.hasKey(counterKey)) {
+	            System.out.println("cache reset...");
+	            redisTemplate.opsForValue().set(counterKey, "0");
+	        }
+	    } catch (DataAccessException e) {
+            // Handle Redis-specific exceptions
+	    	System.out.println("Error accessing Redis cache:" + e);
+            throw new RuntimeException("Error accessing Redis cache", e); // Or rethrow a custom exception
+        }
+	    catch (Exception e) {
+	        // Handle general exceptions
+            System.out.println("Error retrieving next entry" + e);
+	        throw new RuntimeException("Error retrieving next entry", e);
+	    }  finally {
+	        lock.unlock();
+	    }
+	}
+
+	private Boolean compareQueueNames(List<String> cachedQueues, List<String> listOfQueues) {
+	    System.out.println("cache compare...");
+	    return cachedQueues.equals(listOfQueues);
+	}*/
+
+    
+    
+    /*
+    @GetMapping("/getNextEntry")
+    public String getNextEntry(@RequestParam String disseminationProfileId) {
+    	Map<Object, Object> cachedQueues = redisTemplate.opsForHash().entries("ISO_CACHE_DISSEMINATION_ROUNDROBIN_"+disseminationProfileId);
+    	if(!cachedQueues.isEmpty()) 
+    		return roundRobinService.getNextEntry(disseminationProfileId, cachedQueues);
+    	else {
+            System.out.println("Cache is NULL/Empty "+cachedQueues);
+            cachedQueues = roundRobinService.initializeCache(disseminationProfileId, queueNames);
+            return roundRobinService.getNextEntry(disseminationProfileId, cachedQueues);
+    	}
+    }*/
+    
+    /*@PostMapping("/add")
+    public ResponseEntity<Void> addValue(@RequestParam String key, @RequestParam String value) {
+        roundRobinService.addValue(key, value);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }*/
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisExampleApplication.class, args);
+    }
 
 }
